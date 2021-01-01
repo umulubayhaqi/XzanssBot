bfcb3cf1bf81ff2c0914f7d2b08d8124e67b27e0
umulubayhaqi <76832437+umulubayhaqi@users.noreply.github.com>
2021-01-01 07:06:41 +0000

    Update index.js

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ const tambahan = require("./lib/tambahan.js")
 const donasi = require("./lib/donasi.js");
 const info = require("./lib/info.js");
 /////////////////
-const BotName = 'REUZ BOT☄'; 
+const BotName = 'XzanssBot☄'; 
 const instagram = 'https://instagram.com/reuz.id'; 
-const telegram = 'https://t.me/reuzzz'; 
+const telegram = 'https://t.me/Reuzz'; 
 const kapanbotaktif = 'KETIKA HANYA DIAKTIFKAN'; 
 const youtube = 'https://www.youtube.com/c/reuz_id';
 //const grupch1 = 'belum ada grup'; 
